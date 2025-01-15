5ffbf1368a3e141ed1c287f0b92c6d5becf6e4db
Paula Fraga <paula.fragapi@elorrieta-errekamari.com>
2025-01-15 12:25:21 +0100

    Clase 15/01

--- ElorAdmin/frontend/src/app/auth/login/login.component.ts
+++ ElorAdmin/frontend/src/app/auth/login/login.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-
 
 @Component({
     selector: 'app-login',
-    imports: [InputTextModule, PasswordModule, ButtonModule, CardModule,],
+    imports: [RouterLink],
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
