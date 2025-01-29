12c8f14b8522cf5efe35d822f52e22dae6ed5f87
in2dm3-d <in2dm3-d@elorrieta.int>
2025-01-29 12:46:16 +0100

    erabiltzaileak CRUD

---
+++ ElorAdmin/frontend/src/app/pages/gehitu-editatu-orria/gehitu-editatu-orria.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { GehituEditatuErabiltzaileComponent } from "../../Components/gehitu-editatu-erabiltzaile/gehitu-editatu-erabiltzaile.component";
+import { MegamenuComponent } from "../../Components/megamenu/megamenu.component";
+
+@Component({
+  selector: 'app-gehitu-editatu-orria',
+  imports: [GehituEditatuErabiltzaileComponent, MegamenuComponent],
+  templateUrl: './gehitu-editatu-orria.component.html',
+  styleUrl: './gehitu-editatu-orria.component.css'
+})
+export class GehituEditatuOrriaComponent {
+
+}
